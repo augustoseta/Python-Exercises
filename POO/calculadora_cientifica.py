dda5a5c483757d98e0f951c413c3bd1b0329bef8
augustoseta <augustoseta97@gmail.com>
2021-07-21 21:02:00 -0300

    Se añadieron nuevos modulos POO

---
+++ POO/calculadora_cientifica.py
@@ -0,0 +1,49 @@
+
+class Calculadora(object):
+    def __init__(self, numero):
+        self.n = numero
+        self.datos = [0 for i in range(numero)]
+
+    def ingresar_dato(self):
+        self.datos = [int(input('Ingresar datos: ' + str(i+1) + '= ')) for i in range(self.n)]
+
+class op_basicas(Calculadora):
+    def __init__(self):
+        Calculadora.__init__(self,2)
+
+    def suma(self):
+        a,b = self.datos
+        s = a + b
+        print(' el resultado es: ', s)
+
+    def resta(self):
+        a,b = self.datos
+        r = a - b
+        print(' el resultado es: ', r)
+
+    def mult(self):
+        a,b = self.datos
+        m = a * b
+        print(' el resultado es: ', m)
+
+    def div(self):
+        a,b = self.datos
+        d = a / b
+        print(' el resultado es: ', d)
+
+class raiz(Calculadora):
+    def __init__(self):
+        Calculadora.__init__(self, 1)
+
+    def r_cuadrada(self):
+        import math
+        a, = self.datos
+        print(' el resultado es: ', math.sqrt(a))
+
+
+
+
+
+#ejemplo = raiz()
+#print(ejemplo.ingresar_dato())
+#print(ejemplo.r_cuadrada())
